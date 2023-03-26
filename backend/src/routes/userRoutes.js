2330240c0fcab61137ec8d70a145d6c926dd6091
Sohel Rana <sohelrana1304@gmail.com>
2023-03-26 20:54:46 +0530

    Added test message

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -8,6 +8,11 @@ import { userAuth } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+router.get("/test2", (req, res) => {
+  res.send("Helo from Route");
+});
+
+
 router.post("/register", userSignUp);
 router.post("/login", userLogin);
 router.put("/profile", userAuth, updateUserProfile);
